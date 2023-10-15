037674c5f9eac9e1257b2a82b10143657fa2617b
Ivan Cardenas <ivan.cardenas@itrocksweb.com>
2023-10-15 18:45:52 -0300

    se corrigio mapa

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -9,10 +9,9 @@ export const Location = () => {
       <h2>¿Dónde y Cuando?</h2>
       <p>Domingo 5 de Noviembre</p>
       <p>Desde las 13:30hs hasta las 18hs</p>
-      <p> </p>
+      <p>Camino Real Presbítero González y Montevideo</p>
 
-      <iframe
-        src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3285.636810087636!2d-58.47067783383789!3d-34.56275002383545!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb67e1706cd25%3A0x827b573793797069!2sParroquia%20Nuestra%20Se%C3%B1ora%20de%20Lourdes!5e0!3m2!1ses!2sar!4v1682447061848!5m2!1ses!2sar'
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.4667242982255!2d-58.59121482429929!3d-34.84449827286888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcdbbbc489c66f%3A0x1e5552c9253e8d54!2sLa%20domenica%20casa%20de%20campo!5e0!3m2!1ses-419!2sar!4v1697406116977!5m2!1ses-419!2sar" 
         //width="600"
         className='map-iframe'
         //height="450"
