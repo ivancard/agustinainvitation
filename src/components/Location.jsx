0bf6a7d7289966acd9dbe6c82ddc1331246ea954
Ivan Cardenas <ivan.cardenas@itrocksweb.com>
2023-10-15 18:29:31 -0300

    optimizacion de fotos

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -9,7 +9,7 @@ export const Location = () => {
       <h2>¿Dónde y Cuando?</h2>
       <p>Domingo 5 de Noviembre</p>
       <p>Desde las 13:30hs hasta las 18hs</p>
-      <p>Parroquia Nuestra señora de Lourdes</p>
+      <p> </p>
 
       <iframe
         src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3285.636810087636!2d-58.47067783383789!3d-34.56275002383545!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb67e1706cd25%3A0x827b573793797069!2sParroquia%20Nuestra%20Se%C3%B1ora%20de%20Lourdes!5e0!3m2!1ses!2sar!4v1682447061848!5m2!1ses!2sar'
